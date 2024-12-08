c0e9b1a9f3d330d4cdd0d758d33c67c738125183
Omer-Faruk-Dursun <omerfarukdursun7@gmail.com>
2024-12-08 16:48:26 +0300

    Initial commit.

---
+++ src/test/java/com/tchefs/quartermaster/QuartermasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tchefs.quartermaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartermasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
