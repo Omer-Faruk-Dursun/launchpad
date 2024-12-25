a452b508edc0a3c9386f6d40833b6b3f06a6d8cd
Omer-Faruk-Dursun <omerfarukdursun7@gmail.com>
2024-12-25 20:38:10 +0300

    Testing

--- src/main/java/com/tchefs/quartermaster/stock/StockController.java
+++ src/main/java/com/tchefs/quartermaster/stock/StockController.java
@@ -1,16 +1,25 @@
 package com.tchefs.quartermaster.stock;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StockController {
 
     @GetMapping("/stock/product")
-    public Product getStock() {
+    public Product getProduct() {
         Product wheat = new Product("Wheat", new WeightUnit(10));
 
         return wheat;
     }
+
+    @PostMapping("/stock/product")
+    public Long addProduct() {
+        Product wheat = new Product("Wheat", new WeightUnit(10));
+
+        return 1L;
+    }
+
 
 }
