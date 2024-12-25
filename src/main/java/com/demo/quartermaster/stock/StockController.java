3d0a1ce84daa8d2386694f77cfd5b98876244855
Omer-Faruk-Dursun <omerfarukdursun7@gmail.com>
2024-12-25 21:35:14 +0300

    workflow_dispatch

--- src/main/java/com/demo/quartermaster/stock/StockController.java
+++ src/main/java/com/demo/quartermaster/stock/StockController.java
@@ -18,7 +18,7 @@ public class StockController {
     public Long addProduct() {
         Product wheat = new Product("Wheat", new WeightUnit(10));
 
-        return 2L;
+        return 3L;
     }
 
 
